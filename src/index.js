54f693070c88fb5d8c06bdea021ece45f21f5f28
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-09-05 21:26:55 +0100

    feat: requisito 1 e config. iniciais

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,24 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talker = await readFile();
+    res.status(200).json(talker);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
